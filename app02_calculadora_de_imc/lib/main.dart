5fa7be0f67e7c06752ea96a06e4284eaba866022
Rafael Araújo <rafael.gloria@crptecnologia.com.br>
2019-10-02 17:47:48 -0300

    Iniciado o projeto do app2 calculadora de IMC

---
+++ app02_calculadora_de_imc/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Calculadora de IMC"
+        ),
+      ),
+    );
+  }
+}
